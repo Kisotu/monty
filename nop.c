508a1670a01f37ef691a7e829b1536bb374b587f
Oloishorua <jacoboloishorua@gmail.com>
2024-01-19 04:04:20 +0300

    task 5 - nop doesn’t do anything.

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *func_nop - doesn’t do anything.
+  *@head: stack head pointer
+  *@counter: line_number
+  *Return: NULL
+ */
+void func_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
