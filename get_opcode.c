79fd7cf154d4a29605935106515f360f4662c1c3
Oloishorua <jacoboloishorua@gmail.com>
2024-01-19 04:05:10 +0300

    matches opcodes to functions

---
+++ get_opcode.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+/**
+* execute - executes the opcode
+* @stack: head linked list - stack
+* @counter: line_counter
+* @file: poiner to monty file
+* @content: line content
+* Return: no return
+*/
+int get_opcod(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+				{"push", func_push}, {"pall", func_pall}, {"pint", func_pint},
+				{"pop", func_pop},
+				{"swap", func_swap},
+				{"add", func_add},
+				{"nop", func_nop},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
+
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	cmdx.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_dlist(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
+}
